bbff1bbab28bdaaa61d61523bda1416af1cbc143
Gabriel Kihlman <gk@sysctl.se>
2024-04-30 16:30:25 +0200

    upgrade to versioneer 0.28
    ..to fix build errors with newer python

--- src/ed25519/__init__.py
+++ src/ed25519/__init__.py
@@ -9,3 +9,6 @@ from .keys import (BadSignatureError, BadPrefixError,
 from ._version import get_versions
 __version__ = str(get_versions()['version'])
 del get_versions
+
+from . import _version
+__version__ = _version.get_versions()['version']
